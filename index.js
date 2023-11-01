1ef05ccde19d3dda0a460321f65236396fb28da7
Austin Vichich <avichich4@gmail.com>
2023-10-31 19:29:21 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(param1 = 'roller-skate') {
+    return `This Saturday, I want to ${param1}!`;
+  }
+saturdayFun();
+
+function mondayWork(param2 = "go to the office") {
+    return `This Monday, I will ${param2}.`
+}
+mondayWork();
+
+function wrapAdjective(wrap = "*") {
+    return function(greeting) {
+       return `You are ${wrap}${greeting}${wrap}!`;
+     };
+   }
+  wrapAdjective("%")("a dedicated programmer");
